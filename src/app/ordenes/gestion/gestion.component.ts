30f82aa411a807554464cba5a96cd1c0f5f71f0f
Administrador@PURISSIMA.ORG <Administrador@PURISSIMA.ORG>
2021-09-02 12:00:07 -0400

    update

--- src/app/ordenes/gestion/gestion.component.ts
+++ src/app/ordenes/gestion/gestion.component.ts
@@ -12,7 +12,8 @@ export class GestionComponent implements OnInit {
   public MAQUINAS;
   public NUEVA_GESTION:boolean = false;
   public FASE = 'IMPRESION';
-  public GESTIONES
+  public GESTIONES;
+  public LAST_ONE;
 
   constructor(private api:RestApiService) { }
 
@@ -64,13 +65,33 @@ export class GestionComponent implements OnInit {
   finalizar(){
 
     let hoy = moment().format('yyyy-MM-DD');
+    let orden =  ''
+    let productos = ''
+    let hojas = ''
 
-
-    let orden =  (<HTMLInputElement>document.getElementById('orden_selected')).value;
+    orden =  (<HTMLInputElement>document.getElementById('orden_selected')).value;
     let Ejemplares = this.TRABAJOS.find(x => x._id == orden);
 
-    let productos = (<HTMLInputElement>document.getElementById('productos_input')).value
-    let hojas = (<HTMLInputElement>document.getElementById('hojas_input')).value
+    productos = (<HTMLInputElement>document.getElementById('productos_input')).value
+    hojas = (<HTMLInputElement>document.getElementById('hojas_input')).value
+
+    let restante = this.GESTIONES.filter(x=> x.orden == orden)
+
+    let long = restante.length
+
+    let _productos = 0;
+    let _hojas = 0;
+
+    if(long <= 0){
+      console.log(orden)
+      let Actual = this.TRABAJOS.find(x=> x._id == orden)
+      console.log(Actual)
+      _productos = Actual.orden.cantidad - Number(productos);
+      _hojas = Actual.orden.paginas - Number(hojas)
+    }else{
+      _productos = restante[long - 1].Rproductos-Number(productos)
+      _hojas = restante[long - 1].Rhojas - Number(hojas)
+    }
 
     let data = {
       orden : orden,
@@ -78,6 +99,8 @@ export class GestionComponent implements OnInit {
       maquina: Ejemplares.maquina._id,
       productos:productos,
       hojas:hojas,
+      Rproductos:_productos,
+      Rhojas:_hojas
     }
 
     this.api.postGestion(data)
@@ -85,8 +108,7 @@ export class GestionComponent implements OnInit {
         (<HTMLInputElement>document.getElementById('productos_input')).value = '';
         (<HTMLInputElement>document.getElementById('hojas_input')).value = '';
         this.modal_nueva_gestion();
-
-        console.log(resp)
+        this.getGestiones();
       })
 
 
@@ -96,14 +118,12 @@ export class GestionComponent implements OnInit {
     this.api.getGestiones()
       .subscribe((resp:any)=>{
         this.GESTIONES = resp
-        console.log(this.GESTIONES)
       })
   }
 
 
   Tarea(){
     let hoy = moment().format('yyyy-MM-DD');
-    let fase = 'IMPRESION'
     
     this.api.getTrabajos()
       .subscribe((resp:any)=>{
@@ -111,14 +131,12 @@ export class GestionComponent implements OnInit {
         let nuevo = resp.filter(x => x.maquina.tipo === this.FASE);
 
         if(nuevo){
-          let final = [];
           let Long = nuevo.length;
           for(let i=0; i<Long; i++){
     
             if(hoy >= nuevo[i].fechaI){
               if(hoy <= nuevo[i].fecha){
                 this.TRABAJOS.push(nuevo[i])
-                console.log(this.TRABAJOS)
               }
             }
     
